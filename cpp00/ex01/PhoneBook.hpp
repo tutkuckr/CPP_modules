5ab48acbca44f618e159f2da69b7c8bdea7adfa6
Tutku Cakir Yilmaz <tcakir-y@f0r6s1.codam.nl>
2026-01-15 16:41:19 +0100

    started ex01

--- cpp00/ex01/PhoneBook.hpp
+++ cpp00/ex01/PhoneBook.hpp
@@ -1,17 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   my_awesome_phonebook.hpp                           :+:      :+:    :+:   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tcakir-y <tcakir-y@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 19:04:51 by tcakir-y          #+#    #+#             */
-/*   Updated: 2025/09/30 19:45:30 by tcakir-y         ###   ########.fr       */
+/*   Updated: 2026/01/15 16:37:43 by tcakir-y         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
+
+/*
+* array of contacts
+* max 8 contacts, delete oldest one and replace with new one. if you add more
+* no dynamic allocation
+*/
+
 
 class PhoneBook {
 	public:
--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tcakir-y <tcakir-y@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 18:25:47 by tcakir-y          #+#    #+#             */
-/*   Updated: 2025/09/30 19:55:37 by tcakir-y         ###   ########.fr       */
+/*   Updated: 2026/01/15 16:41:03 by tcakir-y         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,33 @@ int	main(void)
 		{
 			test.add();
 		}
+		else if (input.compare("SEARCH"))
+		{
+			test.search();
+		}
 		std::cout << "Enter one of the following commands:\n1.ADD\n2.SEARCH\n3.EXIT\n";
 		std::cin >> input;
 	}
 	std::cout << "Exited\n";
 	return (0);
 }
+
+/*
+anything that will always be used inside a class is private, 
+and that anything that can be used outside a class is public
+*/
+
+/*
+ADD:	*first name, last name, nickname, phone number, and darkest secret. 
+		*A saved contact can’t have empty fields.
+
+SEARCH:	*Display the saved contacts as a list of 4 columns: 
+		index, first name, last name and nickname.
+		*each column 10 characters, pipe separates, right aligned
+		if text is longer, replace last displayable char by '.'
+		if index entered is out of range/wrong, define relevant behaviour
+
+EXIT: *program quits and contacts are lost!
+
+ignore any other input
+*/
