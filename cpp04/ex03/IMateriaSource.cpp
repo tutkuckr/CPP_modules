2e289798bdf1c08f65881a5662e33887aa6bb870
Tutku Cakir Yilmaz <tcakir-y@f0r1s9.codam.nl>
2026-04-07 17:35:29 +0200

    added constructors, cpp04, ex03

--- cpp04/ex03/IMateriaSource.cpp
+++ cpp04/ex03/IMateriaSource.cpp
@@ -6,7 +6,28 @@
 /*   By: tcakir-y <tcakir-y@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/07 10:26:32 by tcakir-y          #+#    #+#             */
-/*   Updated: 2026/04/07 10:26:34 by tcakir-y         ###   ########.fr       */
+/*   Updated: 2026/04/07 17:22:12 by tcakir-y         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "IMateriaSource.hpp"
+
+IMateriaSource::IMateriaSource()
+{
+
+}
+
+IMateriaSource::IMateriaSource(const IMateriaSource &other)
+{
+
+}
+
+IMateriaSource &IMateriaSource::operator=(const IMateriaSource &other)
+{
+
+}
+
+IMateriaSource::~IMateriaSource()
+{
+
+}
--- cpp04/ex03/MateriaSource.cpp
+++ cpp04/ex03/MateriaSource.cpp
@@ -6,7 +6,38 @@
 /*   By: tcakir-y <tcakir-y@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/07 11:39:29 by tcakir-y          #+#    #+#             */
-/*   Updated: 2026/04/07 11:39:57 by tcakir-y         ###   ########.fr       */
+/*   Updated: 2026/04/07 17:34:53 by tcakir-y         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "MateriaSource.hpp"
+
+MateriaSource::MateriaSource() : IMateriaSource()
+{
+
+}
+
+MateriaSource::MateriaSource(const MateriaSource &other)  : IMateriaSource(other)
+{
+
+}
+
+MateriaSource &MateriaSource::operator=(const MateriaSource &other)
+{
+
+}
+
+MateriaSource::~MateriaSource()
+{
+
+}
+
+void MateriaSource::learnMateria(AMateria*)
+{
+
+}
+
+AMateria* MateriaSource::createMateria(std::string const & type)
+{
+
+}
