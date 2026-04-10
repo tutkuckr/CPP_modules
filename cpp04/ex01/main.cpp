d1fe61973ce6a974d2b34204f117f1bc07df48b4
Tutku Cakir Yilmaz <tcakir-y@f0r2s9.codam.nl>
2026-04-10 12:46:52 +0200

    edited main for cpp04 ex01

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tutku <tutku@student.42.fr>                +#+  +:+       +#+        */
+/*   By: tcakir-y <tcakir-y@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/22 16:53:25 by tutku             #+#    #+#             */
-/*   Updated: 2026/03/27 22:21:35 by tutku            ###   ########.fr       */
+/*   Updated: 2026/04/10 12:46:15 by tcakir-y         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,24 +25,21 @@ static void printTitle(const std::string &title)
 
 int main()
 {
-	printTitle("1. BASIC POLYMORPHISM TEST");
+	printTitle("1. BASIC TEST");
 
 	const Animal *meta = new Animal();
 	const Animal *j = new Dog();
 	const Animal *i = new Cat();
 
-	std::cout << "meta type: " << meta->getType() << std::endl;
-	std::cout << "j type: " << j->getType() << std::endl;
-	std::cout << "i type: " << i->getType() << std::endl;
-
 	std::cout << std::endl;
+
 	std::cout << "Sounds:" << std::endl;
 	meta->makeSound();
 	j->makeSound();
 	i->makeSound();
 
 	std::cout << std::endl;
-	std::cout << "Deleting through Animal pointers..." << std::endl;
+	std::cout << "Deleting..." << std::endl;
 	delete meta;
 	delete j;
 	delete i;
@@ -67,7 +64,7 @@ int main()
 	}
 
 	std::cout << std::endl;
-	std::cout << "Deleting all animals in array..." << std::endl;
+	std::cout << "Deleting..." << std::endl;
 	for (int k = 0; k < size; k++)
 		delete animals[k];
 
@@ -77,7 +74,7 @@ int main()
 	basicDog.setIdea(0, "Chase the postman");
 	basicDog.setIdea(1, "Eat food");
 	basicDog.setIdea(2, "Sleep");
-
+	std::cout << std::endl;
 	std::cout << "basicDog idea[0]: " << basicDog.getIdea(0) << std::endl;
 	std::cout << "basicDog idea[1]: " << basicDog.getIdea(1) << std::endl;
 	std::cout << "basicDog idea[2]: " << basicDog.getIdea(2) << std::endl;
@@ -88,7 +85,7 @@ int main()
 	basicCat.setIdea(0, "Ignore humans");
 	basicCat.setIdea(1, "Break stuff");
 	basicCat.setIdea(2, "Sleep 19 hours");
-
+	std::cout << std::endl;
 	std::cout << "basicCat idea[0]: " << basicCat.getIdea(0) << std::endl;
 	std::cout << "basicCat idea[1]: " << basicCat.getIdea(1) << std::endl;
 	std::cout << "basicCat idea[2]: " << basicCat.getIdea(2) << std::endl;
@@ -96,60 +93,68 @@ int main()
 	printTitle("5. DOG COPY CONSTRUCTOR DEEP COPY TEST");
 
 	Dog originalDog;
+
+	std::cout << std::endl;
 	originalDog.setIdea(0, "Original dog idea 0");
 	originalDog.setIdea(1, "Original dog idea 1");
 	originalDog.setIdea(2, "Original dog idea 2");
+	std::cout << std::endl;
 
 	Dog copiedDog(originalDog);
-
+	std::cout << std::endl;
+	
 	std::cout << "Before changing original:" << std::endl;
 	std::cout << "originalDog idea[0]: " << originalDog.getIdea(0) << std::endl;
 	std::cout << "copiedDog idea[0]:   " << copiedDog.getIdea(0) << std::endl;
-
+	
 	originalDog.setIdea(0, "Changed original dog idea 0");
-
 	std::cout << std::endl;
+
 	std::cout << "After changing original:" << std::endl;
 	std::cout << "originalDog idea[0]: " << originalDog.getIdea(0) << std::endl;
 	std::cout << "copiedDog idea[0]:   " << copiedDog.getIdea(0) << std::endl;
-
 	std::cout << std::endl;
 
 	printTitle("6. CAT COPY CONSTRUCTOR DEEP COPY TEST");
 
 	Cat originalCat;
+
+	std::cout << std::endl;
 	originalCat.setIdea(0, "Original cat idea 0");
 	originalCat.setIdea(1, "Original cat idea 1");
 	originalCat.setIdea(2, "Original cat idea 2");
+	std::cout << std::endl;
 
 	Cat copiedCat(originalCat);
+	std::cout << std::endl;
 
 	std::cout << "Before changing original:" << std::endl;
 	std::cout << "originalCat idea[1]: " << originalCat.getIdea(1) << std::endl;
 	std::cout << "copiedCat idea[1]:   " << copiedCat.getIdea(1) << std::endl;
 
 	originalCat.setIdea(1, "Changed original cat idea 1");
-
 	std::cout << std::endl;
+
 	std::cout << "After changing original:" << std::endl;
 	std::cout << "originalCat idea[1]: " << originalCat.getIdea(1) << std::endl;
-	std::cout << "copiedCat idea[1]:   " << copiedCat.getIdea(1) << std::endl;
-
+	std::cout << "copiedCat idea[1]:   " << copiedCat.getIdea(1) << std::endl; //copiedCat should keep old value
 	std::cout << std::endl;
-	std::cout << "If deep copy is correct, copiedCat should keep old value." << std::endl;
 
 	printTitle("7. DOG COPY ASSIGNMENT DEEP COPY TEST");
 
 	Dog dogA;
 	Dog dogB;
 
+	std::cout << std::endl;
 	dogA.setIdea(0, "DogA first idea");
 	dogA.setIdea(1, "DogA second idea");
 	dogB.setIdea(0, "DogB old idea");
-
+	std::cout << std::endl;
+	
 	std::cout << "Before assignment:" << std::endl;
 	std::cout << "dogA idea[0]: " << dogA.getIdea(0) << std::endl;
 	std::cout << "dogB idea[0]: " << dogB.getIdea(0) << std::endl;
+	std::cout << std::endl;
 
 	dogB = dogA;
 
@@ -172,13 +177,16 @@ int main()
 	Cat catA;
 	Cat catB;
 
+	std::cout << std::endl;
 	catA.setIdea(0, "CatA first idea");
 	catA.setIdea(1, "CatA second idea");
 	catB.setIdea(0, "CatB old idea");
+	std::cout << std::endl;
 
 	std::cout << "Before assignment:" << std::endl;
 	std::cout << "catA idea[0]: " << catA.getIdea(0) << std::endl;
 	std::cout << "catB idea[0]: " << catB.getIdea(0) << std::endl;
+	std::cout << std::endl;
 
 	catB = catA;
 
@@ -201,9 +209,11 @@ int main()
 	Animal *polyDog = new Dog();
 	Animal *polyCat = new Cat();
 
+	std::cout << std::endl;
 	std::cout << "Created polyDog and polyCat as Animal*" << std::endl;
-	std::cout << "Deleting them now..." << std::endl;
-
+	std::cout << "Deleting now..." << std::endl;
+	std::cout << std::endl;
+	
 	delete polyDog;
 	delete polyCat;
 
